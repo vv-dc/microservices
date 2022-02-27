f469c014435a3c638c5856fc9204688bf047b3e4
ShkarupaDC <dimon.shkarupa@gmail.com>
2022-02-27 22:53:39 +0200

    Add simple client app, dockerize it

---
+++ client/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    const fetchData = async () => {
+        const url = `${process.env.BACKEND_HOST}/api`;
+        const result = await fetch(url);
+        return await result.json();
+    };
+    const dataPromise = fetchData();
+</script>
+
+<main>
+    {#await dataPromise}
+        <p>Loading data...</p>
+    {:then data}
+        <p>{JSON.stringify(data)}</p>
+    {:catch error}
+        <p style="color: red;">Error occured: {error.message}</p>
+    {/await}
+</main>
